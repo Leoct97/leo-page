f480649b19919c040aa4e5a8e5224fe94c09f58f
Leoct97 <lecota97@gmail.com>
2022-10-31 14:25:34 -0600

    me falto la coma :s

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://leoct97.github.io'
+  base: 'https://leoct97.github.io/',
   plugins: [react()]
 })
